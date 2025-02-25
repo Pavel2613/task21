46898ddb2c55ba8fcf754607d1e9d96a16b72ffc
Pavel Lebedev <p.lebedev8888@gmail.com>
2025-02-25 20:05:33 +0300

    modification

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Уважаемый пассажир!");
 
-        int ticket_price = 13676; // стоимость билета
-        int one_mile = 20; // стоимость одной мили
-        int miles = ticket_price / one_mile; // расчет количества милей
+        int ticketPrice = 13676; // стоимость билета
+        int oneMile = 20; // стоимость одной мили
+        int miles = ticketPrice / oneMile; // расчет количества милей
 
-        System.out.println("За билет стоимостью " + ticket_price + " рублей Вам начислено " + miles + " миль.");
+        System.out.println("За билет стоимостью " + ticketPrice + " рублей Вам начислено " + miles + " миль.");
 
     }
 }
